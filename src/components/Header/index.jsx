5e2689b00629b0fd344b732c2e57b4d669e3b17d
Jesus <alex.voloma2000@yandex.ru>
2021-03-11 01:06:41 +0300

    Приложение «Диалог»
    Шапка приложения

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserInfo from "./UserInfo";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <UserInfo />
+    </div>
+  );
+};
+
+export default Header;
